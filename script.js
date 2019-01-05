87eeb4532bf2b702a397c6d2841996dddf268bab
Ken <ziipo@hotmail.com>
2019-01-04 20:26:01 -0800

    updating weather with for loop

--- script.js
+++ script.js
@@ -34,7 +34,7 @@
 		  }
 	}
 
-
+// 
 
 
 // weather stuff
@@ -53,6 +53,10 @@
 	let date3 = document.getElementById("date3");
 	let date4 = document.getElementById("date4");
 	let date5 = document.getElementById("date5");
+	const dates = document.getElementsByClassName("date");
+	const icons = document.getElementsByClassName("fIcon");
+	const temps = document.getElementsByClassName("temps");
+	const winds = document.getElementsByClassName("winds");
 	let icon1 = document.getElementById("icon1");
 	let temperature1 = document.getElementById("temp1");
 	let wind1 = document.getElementById("wind1");
@@ -107,42 +111,59 @@
 		jsonObject = JSON.parse(response);
 			cityName2.innerHTML = "Forecast for " + jsonObject.city.name;
 		console.log(jsonObject);
-		// day 1 forecast update
-		day1 = 0;
-		UTCdate1 = new Date((jsonObject.list[day1].dt)*1000);
-		// date1.innerHTML = days[(UTCdate1.getDay())] + " (today)" + "<br>" + months[(UTCdate1.getMonth())] + " " + UTCdate1.getDate();
-		date1.innerHTML = "Today" + "<br>" + months[(UTCdate1.getMonth())] + " " + UTCdate1.getDate();
-		icon1.src = "https://openweathermap.org/img/w/" + jsonObject.list[day1].weather[0].icon + ".png";
-		temp1.innerHTML = (jsonObject.list[day1].main.temp).toFixed(0) + "° F";
-		wind1.innerHTML = (jsonObject.list[day1].wind.speed).toFixed(0) + " mph";
 		
-		day2 = 8;
-		UTCdate2 = new Date((jsonObject.list[day2].dt)*1000);
-		date2.innerHTML = days[(UTCdate2.getDay())] + "<br>" + months[(UTCdate2.getMonth())] + " " + UTCdate2.getDate();
-		icon2.src = "https://openweathermap.org/img/w/" + jsonObject.list[day2].weather[0].icon + ".png";
-		temp2.innerHTML = (jsonObject.list[day2].main.temp).toFixed(0) + "° F";
-		wind2.innerHTML = (jsonObject.list[day2].wind.speed).toFixed(0) + " mph";
 
-		day3 = 16;
-		UTCdate3 = new Date((jsonObject.list[day3].dt)*1000);
-		date3.innerHTML = days[(UTCdate3.getDay())] + "<br>" + months[(UTCdate3.getMonth())] + " " + UTCdate3.getDate();
-		icon3.src = "https://openweathermap.org/img/w/" + jsonObject.list[day3].weather[0].icon + ".png";
-		temp3.innerHTML = (jsonObject.list[day3].main.temp).toFixed(0) + "° F";
-		wind3.innerHTML = (jsonObject.list[day3].wind.speed).toFixed(0) + " mph";
+		// old code - before I optimized with the for loop that follows
+		// day1 = 0;
+		// UTCdate1 = new Date((jsonObject.list[day1].dt)*1000);
+		// // date1.innerHTML = days[(UTCdate1.getDay())] + " (today)" + "<br>" + months[(UTCdate1.getMonth())] + " " + UTCdate1.getDate();
+		// date1.innerHTML = "Today" + "<br>" + months[(UTCdate1.getMonth())] + " " + UTCdate1.getDate();
+		// icon1.src = "https://openweathermap.org/img/w/" + jsonObject.list[day1].weather[0].icon + ".png";
+		// temp1.innerHTML = (jsonObject.list[day1].main.temp).toFixed(0) + "° F";
+		// wind1.innerHTML = (jsonObject.list[day1].wind.speed).toFixed(0) + " mph";
+		
+		// day2 = 8;
+		// UTCdate2 = new Date((jsonObject.list[day2].dt)*1000);
+		// date2.innerHTML = days[(UTCdate2.getDay())] + "<br>" + months[(UTCdate2.getMonth())] + " " + UTCdate2.getDate();
+		// icon2.src = "https://openweathermap.org/img/w/" + jsonObject.list[day2].weather[0].icon + ".png";
+		// temp2.innerHTML = (jsonObject.list[day2].main.temp).toFixed(0) + "° F";
+		// wind2.innerHTML = (jsonObject.list[day2].wind.speed).toFixed(0) + " mph";
 
-		day4 = 24;
-		UTCdate4 = new Date((jsonObject.list[day4].dt)*1000);
-		date4.innerHTML = days[(UTCdate4.getDay())] + "<br>" + months[(UTCdate4.getMonth())] + " " + UTCdate4.getDate();
-		icon4.src = "https://openweathermap.org/img/w/" + jsonObject.list[day4].weather[0].icon + ".png";
-		temp4.innerHTML = (jsonObject.list[day4].main.temp).toFixed(0) + "° F";
-		wind4.innerHTML = (jsonObject.list[day4].wind.speed).toFixed(0) + " mph";
+		// day3 = 16;
+		// UTCdate3 = new Date((jsonObject.list[day3].dt)*1000);
+		// date3.innerHTML = days[(UTCdate3.getDay())] + "<br>" + months[(UTCdate3.getMonth())] + " " + UTCdate3.getDate();
+		// icon3.src = "https://openweathermap.org/img/w/" + jsonObject.list[day3].weather[0].icon + ".png";
+		// temp3.innerHTML = (jsonObject.list[day3].main.temp).toFixed(0) + "° F";
+		// wind3.innerHTML = (jsonObject.list[day3].wind.speed).toFixed(0) + " mph";
 
-		day5 = 32;
-		UTCdate5 = new Date((jsonObject.list[day5].dt)*1000);
-		date5.innerHTML = days[(UTCdate5.getDay())] + "<br>" + months[(UTCdate5.getMonth())] + " " + UTCdate5.getDate();
-		icon5.src = "https://openweathermap.org/img/w/" + jsonObject.list[day5].weather[0].icon + ".png";
-		temp5.innerHTML = (jsonObject.list[day5].main.temp).toFixed(0) + "° F";
-		wind5.innerHTML = (jsonObject.list[day5].wind.speed).toFixed(0) + " mph";
+		// day4 = 24;
+		// UTCdate4 = new Date((jsonObject.list[day4].dt)*1000);
+		// date4.innerHTML = days[(UTCdate4.getDay())] + "<br>" + months[(UTCdate4.getMonth())] + " " + UTCdate4.getDate();
+		// icon4.src = "https://openweathermap.org/img/w/" + jsonObject.list[day4].weather[0].icon + ".png";
+		// temp4.innerHTML = (jsonObject.list[day4].main.temp).toFixed(0) + "° F";
+		// wind4.innerHTML = (jsonObject.list[day4].wind.speed).toFixed(0) + " mph";
+
+		// day5 = 32;
+		// UTCdate5 = new Date((jsonObject.list[day5].dt)*1000);
+		// date5.innerHTML = days[(UTCdate5.getDay())] + "<br>" + months[(UTCdate5.getMonth())] + " " + UTCdate5.getDate();
+		// icon5.src = "https://openweathermap.org/img/w/" + jsonObject.list[day5].weather[0].icon + ".png";
+		// temp5.innerHTML = (jsonObject.list[day5].main.temp).toFixed(0) + "° F";
+		// wind5.innerHTML = (jsonObject.list[day5].wind.speed).toFixed(0) + " mph";
+
+		for (var i = 0; i < 5; i++) {
+			console.log("looped once")
+                dayX = i * 8;
+                UTCdate = new Date((jsonObject.list[dayX].dt)*1000);
+                if (i === 0) {
+                	UTCdate1 = new Date((jsonObject.list[dayX].dt)*1000);
+                	dates[i].innerHTML = "Today" + "<br>" + months[(UTCdate.getMonth())] + " " + UTCdate.getDate();
+	                } else {
+	                dates[i].innerHTML = days[(UTCdate.getDay())] + "<br>" + months[(UTCdate.getMonth())] + " " + UTCdate.getDate();
+	                }
+				icons[i].src = "https://openweathermap.org/img/w/" + jsonObject.list[dayX].weather[0].icon + ".png";
+				temps[i].innerHTML = (jsonObject.list[dayX].main.temp).toFixed(0) + "° F";
+				winds[i].innerHTML = (jsonObject.list[dayX].wind.speed).toFixed(0) + " mph";    
+            }
 	}
 
 	function httpRequestAsync(url, callback)
